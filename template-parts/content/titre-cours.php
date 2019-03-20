7e683f0f42204383a674fa84564d2f0c2d9fc604
Carolanne <myridya@hotmail.com>
2019-03-20 11:01:13 -0400

    Commit 3 : cliquer sur une image mene a article relier

--- template-parts/content/titre-cours.php
+++ template-parts/content/titre-cours.php
@@ -22,6 +22,6 @@ echo '<div class="entry-title'
  . '  "><a href="'; 
 echo esc_url( get_permalink()) . '"';
 echo ' rel="bookmark"><h3>' . $sigle .'</h3><h4>' . $titre . '</h4></a></div>';
-// the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+//the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 ?>
 
