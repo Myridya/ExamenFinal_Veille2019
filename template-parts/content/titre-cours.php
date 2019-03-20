4fdf0ed363d2047dd90728c3658ec4c0fb50deeb
Carolanne <myridya@hotmail.com>
2019-03-20 11:22:38 -0400

    Commit 4 : Image pleine grosseur lorsque dans article

--- template-parts/content/titre-cours.php
+++ template-parts/content/titre-cours.php
@@ -14,7 +14,9 @@
 <?php
 
 $sigle = substr(get_the_title(),0,7);
-$session = substr($sigle,4,1);
+//$session = substr($sigle,4,1);
+$session = substr($sigle,5,1);
+
 
 $titre = substr(get_the_title(),7);
 echo '<div class="entry-title'
@@ -22,6 +24,6 @@ echo '<div class="entry-title'
  . '  "><a href="'; 
 echo esc_url( get_permalink()) . '"';
 echo ' rel="bookmark"><h3>' . $sigle .'</h3><h4>' . $titre . '</h4></a></div>';
-//the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+// the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 ?>
 
